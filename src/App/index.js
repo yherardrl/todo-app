0c60c2f10e14fbdf9c3a37e5c43e044619d2cfd6
yherard <yherard.rl@gmail.com>
2022-04-01 12:45:43 -0300

    aplicación funcionando con local storage

--- src/App/index.js
+++ src/App/index.js
@@ -1,18 +1,31 @@
 import React from "react";
 import { AppUi } from "./AppUi";
 
-const defaultTodos = [
-  { text: "cortar cebolla", completed: true },
-  { text: "Tomar curso intro React", completed: false },
-  { text: "llorar con la llorona", completed: false },
-  { text: "LALALALALA", completed: true },
-];
+// const defaultTodos = [
+//   { text: "cortar cebolla", completed: true },
+//   { text: "Tomar curso intro React", completed: false },
+//   { text: "llorar con la llorona", completed: false },
+//   { text: "LALALALALA", completed: true },
+// ];
+
+const useLocalStorage = () => {};
 
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const localStorageTodos = localStorage.getItem("V1_TODOS");
+  let parsedTodos;
+
+  if (!localStorageTodos) {
+    localStorage.setItem("V1_TODOS", JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState("");
 
   const completedTodos = todos.filter((todo) => !!todo.completed).length;
+  console.log(todos);
   const totalTodos = todos.length;
 
   let searchedTodos = [];
@@ -20,12 +33,19 @@ function App() {
   if (!searchValue >= 1) {
     searchedTodos = todos;
   } else {
+    console.log(todos);
     searchedTodos = todos.filter((todo) => {
       const todoText = todo.text.toLocaleLowerCase();
       const searchText = searchValue.toLocaleLowerCase();
       return todoText.includes(searchText);
     });
   }
+
+  const saveTodos = (newTodo) => {
+    const strinfiedTodos = JSON.stringify(newTodo);
+    localStorage.setItem("V1_TODOS", strinfiedTodos);
+    setTodos(newTodo);
+  };
 
   const completeTodo = (text) => {
     // todoIndex finds position in the array
@@ -35,7 +55,7 @@ function App() {
     // changes the completed atribute to true
     newTodos[todoIndex].completed = true;
     // through the funtion setTodos update the state
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   const deleteTodo = (text) => {
@@ -45,7 +65,7 @@ function App() {
     const newTodos = [...todos];
     // we apply the splice method to the newTodos array to remove the index
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   return (
